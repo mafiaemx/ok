93d1486315439a11161552805f8505cb87fd4537
Anastasiia Fihas <Anastasiia.Fihas@lnu.edu.ua>
2026-04-02 16:05:51 +0300

    змінено RK4Service та LogisticsService

--- Service/RK4Service.cs
+++ Service/RK4Service.cs
@@ -1,4 +1,6 @@
-﻿namespace ok.Service
+﻿using ok.Models;
+
+namespace ok.Service
 {
     public class Rk4Service
     {
@@ -6,25 +8,45 @@
         {
             return -rate * (y / 100.0);
         }
-        public double RungeKutta4(double currentStock, double consumptionRate, double timeAhead)
+        public double RungeKutta4(List<Product> products, double coefConsumption, double timeAhead)
         {
+            var sortedProducts=products.OrderBy(p => p.ExpirationDate ?? DateOnly.MaxValue).ToList();
             double h = 0.1;
             double t = 0;
-            double y = currentStock;
+            double totalAmount = (double)sortedProducts.Sum(p => p.Amount);
             while (t < timeAhead)
             {
-                double k1 = F(t, y, consumptionRate);
-                double k2 = F(t + h / 2, y + h * k1 / 2, consumptionRate);
-                double k3 = F(t + h / 2, y + h * k2 / 2, consumptionRate);
-                double k4 = F(t + h, y + h * k3, consumptionRate);
-                y += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+                double k1 = F(t, totalAmount, coefConsumption);
+                double k2 = F(t + h / 2, totalAmount + h * k1 / 2, coefConsumption);
+                double k3 = F(t + h / 2, totalAmount + h * k2 / 2, coefConsumption);
+                double k4 = F(t + h, totalAmount + h * k3, coefConsumption);
+                totalAmount += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
                 t += h;
-                if (y <= 0)
+                if (totalAmount <= 0)
                 {
-                    return 0;
+                    return totalAmount=0;
                 }
             }
-            return y;
+            double finalvalue= totalAmount;
+            for (int i = sortedProducts.Count - 1; i >= 0; i--)
+            {
+                double originAmount=(double)sortedProducts[i].Amount;
+                if (finalvalue > 0)
+                {
+                   int amountRemains=Math.Min((int)originAmount, (int)finalvalue);
+                    sortedProducts[i].Amount -= (decimal)amountRemains;
+                    finalvalue -= amountRemains;
+                }
+                if (sortedProducts[i].ExpirationDate.HasValue && sortedProducts[i].ExpirationDate.Value.ToDateTime(new TimeOnly(0, 0)) <= DateTime.Now.Add(TimeSpan.FromDays(timeAhead)))
+                {
+                    finalvalue -= (double)sortedProducts[i].Amount;
+                }
+                else
+                {
+                    sortedProducts[i].Amount = 0;
+                }
+            }
+            return totalAmount;
         }
     }
 }
