b73cc2ca156124b3f7b09227e5be5de0d73ecda8
mafiaemx <olya.mandryka@gmail.com>
2026-04-02 12:45:47 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ok.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
